5c97ca514e5b58df9d9a45c90b793926322f94a8
DontUnderstandCode <cathel96@hotmail.co.uk>
2021-12-09 12:30:31 +0000

    pUSHANIMATIONS WORKING  and obstacles moving and stuff

--- This Hard Land dmm/Assets/_Scripts/Managers/ObstacleManager.cs
+++ This Hard Land dmm/Assets/_Scripts/Managers/ObstacleManager.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour
 {
-    public bool isRightPlace;      //True when the player si within the trigger space for an obstacle
-
-
-    public bool plane1Mvd;        //Relates to the first puzzle piece
+    public string obstacleName;
 
 
 }
